8700af19f68565d6e60768dd54b6a4122171e616
Andreas Weber <andy.weber.aw@gmail.com>
2015-01-03 12:01:14 +0100

    Support all term in gl2ps-renderer, add docstring

--- gl_print.cc
+++ gl_print.cc
@@ -1,4 +1,4 @@
-// Copyright (C) 2014 Andreas Weber <andy.weber.aw@gmail.com>
+// Copyright (C) 2015 Andreas Weber <andy.weber.aw@gmail.com>
 //
 // This program is free software; you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
@@ -32,14 +32,25 @@
 
 DEFUN_DLD(gl_print, args, nargout,
           "-*- texinfo -*-\n\
-@deftypefn {Loadable Function} gl_print (@var{h}, @var{filename})\n\
+@deftypefn {Loadable Function} gl_print (@var{h}, @var{filename}, @var{term})\n\
 Print figure @var{h} using OSMesa and gl2ps.\n\
+\n\
+Valid options for @var{term}, which can be concatenated in one string, are:\n\
+\n\
+@table @asis\n\
+@item @qcode{eps}, @qcode{pdf}, @qcode{ps}, @qcode{svg}, @qcode{pgf}, @qcode{tex}\n\
+Select output format\n\
+@item @qcode{is2D}\n\
+Use GL2PS_SIMPLE_SORT instead of GL2PS_BSP_SORT as Z-depth sorting algorithm\n\
+@item @qcode{notext}\n\
+Don't render text\n\
+@end table\n\
 @end deftypefn")
 {
   octave_value_list retval;
   int nargin = args.length ();
 
-  if (nargin != 2)
+  if (nargin != 3)
     {
       print_usage();
       return retval;
@@ -51,22 +62,31 @@ Print figure @var{h} using OSMesa and gl2ps.\n\
     {
       figure::properties& fp =
         dynamic_cast<figure::properties&> (fobj.get_properties ());
-      
+
       bool internal = true;
       Matrix bb = fp.get_boundingbox (internal);
 
       int Width = bb(2);
       int Height = bb(3);
-      
+
       std::string filename = args(1).string_value ();
       if (error_state)
         {
           error ("Filename has to be a string");
           return retval;
         }
-    
-      octave_stdout << "Width=" << Width << " Height=" << Height << " Filename=" << filename << std::endl;
-      octave_stdout << "Output format is SVG, no matter which extension filename has" << std::endl;
+
+      std::string term = args(2).string_value ();
+      if (error_state)
+        {
+          error ("Term has to be a string");
+          return retval;
+        }
+
+      octave_stdout << "Width=" << Width
+                    << " Height=" << Height
+                    << " Filename=" << filename
+                    << " Term=" << term << std::endl;
 
       OSMesaContext ctx;
       void *buffer;
@@ -98,22 +118,23 @@ Print figure @var{h} using OSMesa and gl2ps.\n\
           printf("OSMesaMakeCurrent failed!\n");
           return retval;
         }
-      
+
       int z, s, a;
       glGetIntegerv(GL_DEPTH_BITS, &z);
       glGetIntegerv(GL_STENCIL_BITS, &s);
       glGetIntegerv(GL_ACCUM_RED_BITS, &a);
       printf("Depth=%d Stencil=%d Accum=%d\n", z, s, a);
 
-      /********************************************************************************/
-      
       FILE *filep;
       filep = fopen (filename.c_str () ,"w");
-      glps_renderer rend (filep, "svg");
-      rend.draw (fobj, "");
-      fclose (filep);
-
-      /********************************************************************************/
+      if (filep)
+        {
+          glps_renderer rend (filep, term);
+          rend.draw (fobj, "");
+          fclose (filep);
+        }
+      else
+        error ("Couldn't create file \"%s\"", filename.c_str ());
 
       /* free the image buffer */
       free( buffer );
@@ -122,7 +143,7 @@ Print figure @var{h} using OSMesa and gl2ps.\n\
       OSMesaDestroyContext( ctx );
     }
   else
-    error ("First param has to be a valid figure handle");
-   
+    error ("H has to be a valid figure handle");
+
   return retval;
 }
