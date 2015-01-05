a9fd1a67261ba0b00918045dfcd7c693852d6bd0
Andreas Weber <andy.weber.aw@gmail.com>
2015-01-05 16:04:06 +0100

    add script to compare plots from different toolkits

---
+++ compare_plots.m
@@ -0,0 +1,31 @@
+## Make some plots with native graphics_toolkit fltk,
+## graphics_toolkit gnuplot and gl_print using OSMesa
+
+close all
+graphics_toolkit fltk
+sombrero
+tic
+print ("-S800,600", "sombrero_fltk.png")
+print ("-S800,600", "sombrero_fltk.svg")
+toc
+
+close all
+graphics_toolkit gnuplot
+sombrero
+tic
+print ("-S800,600", "sombrero_gnuplot.png")
+print ("-S800,600", "sombrero_gnuplot.svg")
+toc
+
+close all
+figure ("visible", "off", "position", [0 0 800 600])
+sombrero
+tic
+gl_print ("sombrero_osmesa.png")
+gl_print ("sombrero_osmesa_gl2ps.svg")
+toc
+
+
+
+
+
